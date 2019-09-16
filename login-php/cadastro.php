f304e8da91e6b5422a769c42d3a934f7e463f11d
Marcelo Amorim <marceloamorimcg@hotmail.com>
2019-09-16 12:02:07 -0300

    Tela de cadastro
    tela de cadstro em php e mysql

--- login-php/cadastro.php
+++ login-php/cadastro.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
     
@@ -9,7 +8,7 @@ session_start();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <title>Sistema de Cadastro - PHP + MySQL - Canal TI</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
@@ -20,35 +19,47 @@ session_start();
         <div class="hero-body">
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Login</h3>
+                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
                     <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
                     <?php
-                    if(isset($_SESSION['nao_autenticado'])):
+                    if($_SESSION['status_cadastro']):
                     ?>
-                    <div class="notification is-danger">
-                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    <div class="notification is-success">
+                      <p>Cadastro efetuado!</p>
+                      <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
                     </div>
                     <?php
                     endif;
-                    unset($_SESSION['nao_autenticado']);
+                    unset($_SESSION['status_cadastro']);
+                    ?>
+                    <?php
+                    if($_SESSION['usuario_existe']):
+                    ?>
+                    <div class="notification is-info">
+                        <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['usuario_existe']);
                     ?>
                     <div class="box">
-                        <form action="login.php" method="POST">
+                        <form action="cadastrar.php" method="POST">
                             <div class="field">
                                 <div class="control">
-                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
-                                </div>
-                            </div>
-
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                    <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
                                 </div>
                             </div>
                             <div class="field">
-                                <a href="cadastro.php">Cadastrar</a>
+                                <div class="control">
+                                    <input name="usuario" type="text" class="input is-large" placeholder="Usuário">
+                                </div>
                             </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
                         </form>
                     </div>
                 </div>
--- login-php/verifica_login.php
+++ login-php/verifica_login.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!$_SESSION['usuario']) {
+if(!$_SESSION['nome']) {
 	header('Location: index.php');
 	exit();
 }
